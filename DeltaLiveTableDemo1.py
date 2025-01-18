7d52056a0c7dc8b718464b3d624df5ed13c9399a
Msahabuddin1 <Msahabuddin1>
2025-01-18 18:58:20 +0000

    Commit & Push

--- DeltaLiveTableDemo1.py
+++ DeltaLiveTableDemo1.py
@@ -15,4 +15,64 @@
 
 # COMMAND ----------
 
+files = dbutils.fs.ls("dbfs:/FileStore/tables/nyctaxidata/dltphase1/dlt_demo_10_jan_2023/storage")
+display(files)
 
+# COMMAND ----------
+
+files = dbutils.fs.ls("dbfs:/FileStore/tables/nyctaxidata/dltphase1/dlt_demo_10_jan_2023/storage/system/events")
+display(files)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM
+# MAGIC delta.`dbfs:/FileStore/tables/nyctaxidata/dltphase1/dlt_demo_10_jan_2023/storage/system/events`
+
+# COMMAND ----------
+
+files = dbutils.fs.ls("dbfs:/FileStore/tables/nyctaxidata/dltphase1/dlt_demo_10_jan_2023/storage/tables/")
+display(files)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show databases
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM raju_chal_dlt_demo_10_jan_2023.vendor_zone_passenger_count
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Upload another yellow trip data file into DBFS location
+# MAGIC dbfs:/FileStore/tables/nyctaxidata/dltlanding/yellowdata/yellow_tripdata
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Feel free to run it a couple more times if desired.
+# MAGIC Following this, run the pipeline again and view the results
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM raju_chal_dlt_demo_10_jan_2023.vendor_zone_passenger_count
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Upload another yellow trip data file into DBFS location
+# MAGIC dbfs:/FileStore/tables/nyctaxidata/dltlanding/yellowdata/yellow_tripdata
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM raju_chal_dlt_demo_10_jan_2023.vendor_zone_passenger_count
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
